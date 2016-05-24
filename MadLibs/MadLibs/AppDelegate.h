ff1b0b12e1b5f15966d43d4d645a1062de576305
Ling <asklingtoby@gmail.com>
2016-05-24 14:50:06 -0400

    set up project

---
+++ MadLibs/MadLibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MadLibs
+//
+//  Created by ling toby on 5/24/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MadLibs/MadLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MadLibs
+//
+//  Created by ling toby on 5/24/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
